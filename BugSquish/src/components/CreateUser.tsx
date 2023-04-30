2b8c63d441f9e7430db5db989b22b9e39f566db2
tun35770 <tun35770@temple.edu>
2023-04-30 18:00:03 -0400

    User cna be added to database

--- BugSquish/src/components/CreateUser.tsx
+++ BugSquish/src/components/CreateUser.tsx
@@ -18,6 +18,18 @@ const CreateUser = () => {
 
     console.log(user);
 
+    fetch('http://localhost:5000/users/add', {
+      method: 'POST',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify(user)
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+
     setUsername('');
   }
 
