37f409a014841767db14a6acf0d0dc502809f3a3
tun35770 <tun35770@temple.edu>
2023-05-18 22:28:43 -0400

    CreateBug navigation

--- BugSquish/src/App.tsx
+++ BugSquish/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={user ? <BugList /> : <Navigate to="/login"/>} />
-          <Route path="/create" element={<CreateBug />} />
+          <Route path="/create" element={user ? <CreateBug /> : <Navigate to="/login" />} />
           <Route path="/edit/:id" element={<EditBug />} />
           <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
           <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
